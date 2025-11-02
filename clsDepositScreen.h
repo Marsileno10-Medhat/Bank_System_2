52bf4c14321f1923496c5444d15aa5e3d546b80b
Marsileno Medhat <marsilenomedhat01@gmail.com>
2025-11-02 04:05:02 +0200

    created the deposit transaction UI & process.

---
+++ clsDepositScreen.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <iostream>
+#include "clsInputValidate.h"
+#include "clsUtil.h"
+#include "clsBankClient.h"
+#include "clsScreen.h"
+using namespace std;
+
+class clsDepositScreen : protected clsScreen {
+
+private:
+    static void _PrintClientCard(clsBankClient Client) {
+        cout << "\n---------------------------------------------------";
+        cout << "\n\t\tClient data:";
+        cout << "\n---------------------------------------------------";
+        cout << "\n- First name       : " << Client.GetFirstName();
+        cout << "\n- Last name        : " << Client.GetLastName();
+        cout << "\n- Full name        : " << Client.GetFullName();
+        cout << "\n- Phone number     : " << Client.GetPhone();
+        cout << "\n- Email address    : " << Client.GetEmail();
+        cout << "\n- Account number   : " << Client.GetAccountNumber();
+        cout << "\n- PIN code         : " << Client.GetPINcode();
+        cout << "\n- Account balance  : " << Client.GetAccountBalance();
+        cout << "\n---------------------------------------------------";
+    }
+
+public:
+    static void ShowDepositScreen() {
+        clsScreen::_PrineScreenHeader("Deposit screen.");
+        string AccountNumber = "";
+
+        while (true) {
+            AccountNumber = clsInputValidate::ReadString("Enter the account number or enter \"back\" to back to the main menu: ");
+            if (clsUtil::IsEqualText(AccountNumber)) {
+                cout << "\nThe transaction has been cancelled.\n";
+                return;
+            }
+            if (clsBankClient::IsClientExist(AccountNumber)) {
+                break;
+            }
+            cout << "\nAccount number is not found. ";
+        }
+        clsBankClient Client = clsBankClient::Find(AccountNumber);
+        _PrintClientCard(Client);
+
+        float DepositAmount = clsInputValidate::ReadValidNumber<float>("\nEnter the deposit amount: ", "\nInvalid number, Enter the deposit amount: ");
+        char ConfirmTransaction = clsInputValidate::ReadChar("\nAre you sure you want to perform this transaction? [Y/N]. ");
+
+        if (toupper(ConfirmTransaction) == 'Y') {
+            Client.Deposit(DepositAmount);
+            cout << "\nThe transactio has been done successfully.\n";
+            cout << "\nNew balance: " << Client.GetAccountBalance() << endl;
+        }
+        else {
+            cout << "\nThe transaction has been cancelled.\n";
+        }
+    }
+};
